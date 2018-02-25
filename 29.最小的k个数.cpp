446b97b9ad6fe352ea4388a8d083ae3c41490b47
qiuqiu <1184228534@qq.com>
2018-02-25 21:24:05 +0800

    Update 29.最小的k个数.cpp

--- 29.最小的k个数.cpp
+++ 29.最小的k个数.cpp
@@ -3,16 +3,33 @@
 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
 
 思路
-堆排序问题
+用最大堆保存最小的k个数, 堆顶是这k个数中最大的, 遍历数组, 如果遇到小于堆顶的, 删除堆顶数字, 让这个数进堆.
+multiset是红黑树实现的, 用来实现最大堆.
 */
 
 class Solution {
 public:
     vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
-        if(k > input.size())
+        if(k < 1 || k > input.size())
             return vector<int>{};
-        make_heap(input.begin(), input.end());
-        sort_heap(input.begin(), input.end());
-        return vector<int>(input.begin(), input.begin() + k);
+        multiset<int, greater<int>> leastK;
+        multiset<int, greater<int>>::iterator it;
+        for(auto i = input.begin(); i != input.end(); i++){
+            if(leastK.size() < k)
+                leastK.insert(*i);
+            else{
+                it = leastK.begin();
+                if(*i < *it){
+                    leastK.erase(it);
+                    leastK.insert(*i);
+                }
+            }
+        }
+        vector<int> res;
+        it = leastK.begin();
+        for(int i = 0; i < k;it++, i++){
+            res.push_back(*it);
+        }
+        return res;
     }
 };
